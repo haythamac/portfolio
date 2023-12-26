28f17f97d13ec23fca7aff8c01ff77f60f025292
haythamac <janssen.uy.dev@gmail.com>
2023-12-27 02:51:09 +0800

    Finished pop up message

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -5,10 +5,10 @@
     <div class="flex flex-col items-start gap-4">
         <span class="uppercase text-blue-700 font-bold text-xl">About</span>
         <div class="text-4xl font-extrabold">
-            Janssen Uy is a Web Developer based in PH.
+            Janssen Uy is a <span class="text-blue-700 font-black">Web Developer</span> based in <span class="text-yellow-500 font-black">PH</span>.
         </div>
-        <p class="text-gray-500">I am a Web Designer, and I'm very passionate and dedicated to my work. 
-            With 20 years experience as a professional Web developer, 
+        <p class="text-gray-500 text-lg">I am a <span class="text-blue-700 font-black">Web Developer</span>, and I'm very passionate and dedicated to my work. 
+            With over <span class="text-blue-700 font-black">4</span> years of experience as a programmer, 
             I have acquired the skills and knowledge necessary to make your project a 
             success.
         </p>
--- resources/views/home/side-bar.blade.php
+++ resources/views/home/side-bar.blade.php
@@ -1,6 +1,6 @@
 <div class="fixed w-64 flex flex-col items-center min-h-screen pb-10 bg-blue-700">
     <div class="mb-64 bg-yellow-500 h-48 w-full flex justify-center items-center ">
-        <img src="/images/ha.png" alt="ahahah" class="rounded-full">
+        <img src="/images/bb.jpg" alt="profile picture" class="rounded-full w-32 h-32 object-cover">
     </div>
     <div>
         <ul class="flex flex-col gap-10">

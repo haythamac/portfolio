ccc1b31672eccfc7d76fc7b7ff073259383be97f
haythamac <janssen.uy.dev@gmail.com>
2023-12-27 17:17:38 +0800

    Fixed popup message bug

--- resources/views/home/main-content.blade.php
+++ resources/views/home/main-content.blade.php
@@ -5,3 +5,19 @@
     @include('home.portfolio')
     @include('home.contact')
 </div>
+
+@if(session()->has('message'))
+    <div x-show="isOpen" x-data="{ isOpen: true }" @click.away="isOpen = false" class="fixed inset-0 bg-black bg-opacity-50 z-50">
+        <div
+            class="fixed border py-16 px-16 bg-white rounded top-1/2 left-1/2 transform -translate-x-1/3 -translate-y-1/2 z-50">
+            <button @click="isOpen = false" class="absolute top-4 right-4 text-gray-500 hover:text-gray-700">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+            </button>
+            @livewire('show-popup-message', ['message' => session()->get('message')])
+        </div>
+    </div>
+@endif
--- resources/views/home/skills.blade.php
+++ resources/views/home/skills.blade.php
@@ -1,6 +1,7 @@
 <div class="flex flex-col justify-start m-auto w-248 mb-16 mt-64">
     <span class="font-bold text-blue-700 uppercase mb-4">Skills</span>
-    <span class="font-extrabold text-3xl mb-16">Experience & Skills</span>
+    <span class="font-extrabold text-3xl mb-8">Experience & Skills</span>
+    <button class="uppercase text-white mb-8 w-1/6 bg-blue-700 hover:bg-yellow-400 hover:text-black p-3 rounded duration-500">Add skill</button>
     <div class="grid grid-cols-2">
         <div class="mb-32 mr-8">
             <div class="font-semibold text-xl mb-16">Development Skills</div>

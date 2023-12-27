1e42a6a8289ef58e1de9cb08816ae4bead07a88b
haythamac <janssen.uy.dev@gmail.com>
2023-12-27 14:48:44 +0800

    Little changes to about section

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -5,10 +5,10 @@
     <div class="flex flex-col items-start gap-4">
         <span class="uppercase text-blue-700 font-bold text-xl">About</span>
         <div class="text-4xl font-extrabold">
-            Janssen Uy is a Web Developer based in PH.
+            Janssen Uy is a <span class="text-blue-700">Web Developer based</span> in <span class="text-yellow-400">PH</span>.
         </div>
-        <p class="text-gray-500">I am a Web Designer, and I'm very passionate and dedicated to my work. 
-            With 20 years experience as a professional Web developer, 
+        <p class="text-gray-500 text-lg">I am a <span class="text-blue-700 font-bold">Web Developer</span>, and I'm very passionate and dedicated to my work. 
+            With <span class="text-blue-700 font-bold">4</span> years experience as a <span class="text-blue-700 font-bold">programmer</span>, 
             I have acquired the skills and knowledge necessary to make your project a 
             success.
         </p>
